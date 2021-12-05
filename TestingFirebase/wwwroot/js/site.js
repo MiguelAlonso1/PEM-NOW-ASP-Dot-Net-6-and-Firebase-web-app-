0789c490493c1d4aed03d85ab732caccd4109077
Miguel Alonso <60105368+MiguelAlonso1@users.noreply.github.com>
2021-12-05 14:46:41 -0500

    Added editable rich-text text areas. Added upload images feature to the create subcategory view. Updated look of the theme with a bootstrap

--- TestingFirebase/wwwroot/js/site.js
+++ TestingFirebase/wwwroot/js/site.js
@@ -125,3 +125,25 @@ $('.remove-preview').on('click', function () {
     previewZone.addClass('hidden');
     reset(dropzone);
 });
+
+
+//resizing text areas
+
+
+//$("textarea").each(function () {
+//    this.setAttribute("style", "height:" + (this.scrollHeight) + "px;overflow-y:hidden;");
+//}).on("input", function () {
+//    this.style.height = "auto";
+//    this.style.height = (this.scrollHeight) + "px";
+//});
+
+const tx = document.getElementsByTagName("textarea");
+for (let i = 0; i < tx.length; i++) {
+    tx[i].setAttribute("style", "height:" + (tx[i].scrollHeight) + "px;overflow-y:hidden;");
+    tx[i].addEventListener("input", OnInput, false);
+}
+
+function OnInput() {
+    this.style.height = "auto";
+    this.style.height = (this.scrollHeight) + "px";
+}
